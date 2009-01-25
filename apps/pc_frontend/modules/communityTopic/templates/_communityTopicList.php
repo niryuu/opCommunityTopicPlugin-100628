42686cfe72203d1914da7b8a43308bcc1e7ffa75
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-25 09:27:52 +0000

    opCommunityTopicPlugin: fixed old link destinations

--- apps/pc_frontend/modules/communityTopic/templates/_communityTopicList.php
+++ apps/pc_frontend/modules/communityTopic/templates/_communityTopicList.php
@@ -1,15 +1,15 @@
-<?php if ($hasPermission) : ?>
+<?php if ($hasPermission): ?>
 <?php use_helper('Date'); ?>
 <tr>
 <th>コミュニティ掲示板</th>
 <td>
 <ul>
-<?php if ($communityTopics) : ?>
-<?php foreach ($communityTopics as $key => $communityTopic) : ?>
+<?php if (count($communityTopics)): ?>
+<?php foreach ($communityTopics as $key => $communityTopic): ?>
 <li>
 <?php echo format_datetime($communityTopic->getUpdatedAt(), 'f'); ?>
 &nbsp;
-<?php echo link_to(sprintf('%s(%d)', $communityTopic->getName(), $communityTopic->countCommunityTopicComments()), 'communityTopic/detail?id='.$communityTopic->getId()); ?>
+<?php echo link_to(sprintf('%s(%d)', $communityTopic->getName(), $communityTopic->countCommunityTopicComments()), 'communityTopic_show', $communityTopic) ?>
 </li>
 <?php endforeach; ?>
 <li><?php echo link_to('もっと読む', 'communityTopic_list_community', $community); ?></li>
--- apps/pc_frontend/modules/communityTopic/templates/listCommunitySuccess.php
+++ apps/pc_frontend/modules/communityTopic/templates/listCommunitySuccess.php
@@ -2,7 +2,7 @@
 
 <?php
 $title = 'トピックを作成する';
-$body = link_to('新規作成', 'communityTopic/edit?community_id='.$community->getId());
+$body = link_to('新規作成', 'communityTopic_new', $community);
 include_box('communityTopicList', $title, $body);
 ?>
 
@@ -11,27 +11,27 @@ include_box('communityTopicList', $title, $body);
 <h3><?php echo 'トピック一覧'; ?></h3>
 </div>
 
-<div class="pagerRelative"><p class="number"><?php echo pager_navigation($pager, 'communityTopic/list?page=%d&community_id='.$community->getId()); ?></p></div>
+<div class="pagerRelative"><p class="number"><?php echo pager_navigation($pager, '@communityTopic_list_community?page=%d&id='.$community->getId()) ?></p></div>
 
 <table><tbody>
 <?php foreach ($pager->getResults() as $topic): ?>
 <tr>
-<th rowspan=2><?php echo format_datetime($topic->getUpdatedAt(), 'f'); ?></th>
-<td><?php echo link_to($topic->getName(), 'communityTopic/detail?id='.$topic->getId()); ?></td>
+<th rowspan=2><?php echo format_datetime($topic->getUpdatedAt(), 'f') ?></th>
+<td><?php echo link_to($topic->getName(), 'communityTopic_show', $topic) ?></td>
 </tr>
 <tr>
 <td class="border-left align-right">
 <?php
 echo link_to('編集', 'communityTopic/edit?id='.$topic->getId());
 echo ' | ';
-echo link_to('もっと見る'.'('.$topic->countCommunityTopicComments().')', 'communityTopic/detail?id='.$topic->getId());
+echo link_to('もっと見る'.'('.$topic->countCommunityTopicComments().')', 'communityTopic_show', $topic);
 ?></td>
 </tr>
 <?php endforeach; ?>
 </tbody>
 </table>
 
-<div class="pagerRelative"><p class="number"><?php echo pager_navigation($pager, 'communityTopic/list?page=%d&community_id='.$community->getId()); ?></p></div>
+<div class="pagerRelative"><p class="number"><?php echo pager_navigation($pager, '@communityTopic_list_community?page=%d&id='.$community->getId()) ?></p></div>
 
 </div>
 </div>

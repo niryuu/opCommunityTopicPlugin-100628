f5ed88a32b7d271206bdb0aff3983b9dfbbfe146
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-03-18 03:07:01 +0000

    opCommunityTopicPlugin:fixed topicCommentListBox on the pc_frontend

--- apps/pc_frontend/modules/communityTopic/templates/_topicCommentListBox.php
+++ apps/pc_frontend/modules/communityTopic/templates/_topicCommentListBox.php
@@ -1,14 +1,12 @@
+<?php if (count($communityTopic)): ?>
 <div id="homeRecentList_<?php echo $gadget->getId() ?>" class="dparts homeRecentList"><div class="parts">
 <div class="partsHeading"><h3><?php echo __('Recently Posted Community Topics') ?></h3></div>
 <div class="block">
-
-<?php if (count($communityTopic)): ?>
 <ul class="articleList">
 <?php foreach ($communityTopic as $topic): ?>
 <li><span class="date"><?php echo op_format_date($topic->getUpdatedAt(), 'XShortDateJa') ?></span><?php echo link_to(op_truncate($topic->getName(), 36).' ('.$topic->countCommunityTopicComments().')', 'communityTopic_show', $topic).' ('.$topic->getCommunity()->getName().')' ?></li>
 <?php endforeach; ?>
 </ul>
-<?php endif; ?>
-
 </div>
 </div></div>
+<?php endif; ?>
